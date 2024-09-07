87d8ffbc5369369ca3f7298890f1e04e87bd1956
Naif <naif@local>
2024-09-07 05:57:05 +0300

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,13 +6,13 @@ chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 755 /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Run database migrations (optional)
-# echo "Running database migrations..."
-# php artisan migrate --force
+ echo "Running database migrations..."
+ php artisan migrate --force
 
 # Run any additional Laravel commands you need (optional)
-# php artisan config:cache
-# php artisan route:cache
-# php artisan view:cache
+ php artisan config:cache
+ php artisan route:cache
+ php artisan view:cache
 
 # Run Apache in the foreground
 echo "Starting Apache..."
